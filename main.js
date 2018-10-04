0ea0061be23fa4c53a859b8745fb9f67bd3ca81a
Christopher Xiong <christopherx@mail.fresnostate.edu>
2018-10-03 18:36:23 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var MyfirstProgram = /** @class */ (function () {
+    function MyfirstProgram() {
+        var argArray = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            argArray[_i] = arguments[_i];
+        }
+        this.printArgs(argArray);
+    }
+    MyfirstProgram.prototype.printArgs = function (argArray) {
+        console.log(argArray);
+    };
+    MyfirstProgram.prototype.runFirst = function () {
+        console.log('this method runs first...');
+    };
+    MyfirstProgram.prototype.runLast = function () {
+        console.log('this method runs last');
+    };
+    return MyfirstProgram;
+}());
+var newInstance = new MyfirstProgram("a", "b", "c");
---
+++ main.ts
@@ -0,0 +1,24 @@
+
+
+class MyfirstProgram {
+
+    constructor(...argArray: Array<any>) {
+        this.printArgs(argArray);
+    }
+
+    printArgs(argArray){
+        console.log(argArray);
+    }
+
+   runFirst(){
+       console.log('this method runs first...');
+   } 
+
+   runLast(){
+       console.log('this method runs last');
+   }
+}
+
+const newInstance = new MyfirstProgram("a", "b", "c");
+
+
